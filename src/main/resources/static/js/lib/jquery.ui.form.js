3956b867d178457ab16d06331a5d41a12227ac05
byron41265 <412650552@qq.com>
2018-07-17 21:51:56 +0800

    limit password

--- src/main/resources/static/js/lib/jquery.ui.form.js
+++ src/main/resources/static/js/lib/jquery.ui.form.js
@@ -246,6 +246,34 @@ $.widget( "ui.validate", {
 					return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
 				}
 			},
+			
+			password: {
+				message : "Password length needs to be 8 to 16 digits and must contain at least two of letters, numbers and symbols",
+				validate : function(value,element) {
+					if(value == null || value.length == 0){
+						return true;
+					}
+					var regUpper = /[a-zA-Z]/;
+					var regNum = /[0-9]/;
+					var regTeShu = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？+-]");
+					var count = 0;
+					if(regUpper.test(value)){
+						++count;
+					}
+					if(regNum.test(value)){
+						++count;
+					}
+					if(regTeShu.test(value)){
+						++count;
+					}
+					if(count<2 || value.length<8 || value.length>16){
+						return false;
+					}else{
+						return true;
+					}
+					
+				}
+			}
 
 			// http://docs.jquery.com/Plugins/Validation/Methods/url
 			url: {
